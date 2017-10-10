8bd98850df0f65651482f94bb0fda1ca671e1cdd
agackovkaw <anton.gackovka@odysseusinc.com>
2017-10-10 21:49:27 +0300

    Extended check whether user can delete analysis file

--- src/main/java/com/odysseusinc/arachne/portal/component/PermissionDslPredicates.java
+++ src/main/java/com/odysseusinc/arachne/portal/component/PermissionDslPredicates.java
@@ -2,9 +2,12 @@ package com.odysseusinc.arachne.portal.component;
 
 import com.odysseusinc.arachne.portal.model.Analysis;
 import com.odysseusinc.arachne.portal.model.AnalysisFile;
+import com.odysseusinc.arachne.portal.model.ParticipantRole;
+import com.odysseusinc.arachne.portal.model.UserStudyExtended;
 import com.odysseusinc.arachne.portal.model.security.ArachneUser;
 import java.util.Objects;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 public class PermissionDslPredicates {
@@ -18,7 +21,17 @@ public class PermissionDslPredicates {
     }
 
     public static <T extends AnalysisFile> Predicate<T> analysisFileAuthorIs(ArachneUser user) {
-        return analysisFile -> Objects.nonNull(analysisFile.getAnalysis().getAuthor()) && Objects.equals(analysisFile.getAnalysis().getAuthor().getId(), user.getId());
+        return analysisFile -> Objects.nonNull(analysisFile.getAnalysis().getAuthor())
+                && Objects.equals(analysisFile.getAnalysis().getAuthor().getId(), user.getId());
+    }
+
+    public static <T extends AnalysisFile> Predicate<T> userIsLeadInvestigator(ArachneUser user) {
+        return analysisFile -> Objects.nonNull(analysisFile.getAnalysis())
+                && analysisFile.getAnalysis().getStudy().getParticipants().stream()
+                .filter(userLink -> Objects.equals(user.getId(), userLink.getUser().getId()))
+                .map(UserStudyExtended::getRole)
+                .collect(Collectors.toSet())
+                .contains(ParticipantRole.LEAD_INVESTIGATOR);
     }
 
     public static <T> Predicate<T> hasRole(ArachneUser user, String role) {
