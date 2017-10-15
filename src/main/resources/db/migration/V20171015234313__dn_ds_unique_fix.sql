8245bad5a6ab5cae4614eff6d9b49c2fee75c93a
Pavel Grafkin <pavgra@gmail.com>
2017-10-15 23:51:34 +0300

    Fixed unique indexes for virtual data sources

---
+++ src/main/resources/db/migration/V20171015234313__dn_ds_unique_fix.sql
@@ -0,0 +1,8 @@
+DROP INDEX IF EXISTS name_not_virtual_uq;
+DROP INDEX IF EXISTS name_uk;
+
+CREATE UNIQUE INDEX IF NOT EXISTS datanodes_not_virtual_name_uq
+  ON datanodes (name) WHERE (is_virtual = FALSE);
+
+CREATE UNIQUE INDEX IF NOT EXISTS data_sources_name_uk
+  ON data_sources (data_node_id, name);
