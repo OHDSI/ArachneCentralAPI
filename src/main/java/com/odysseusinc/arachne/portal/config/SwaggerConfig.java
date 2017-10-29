82bc6d4694f0a92bf96b094fc226359ca4be8d88
Alexandr Ryabokon <mr.alexandr.ryabokon@gmail.com>
2017-10-29 20:56:07 +0300

    ARACHNE-1500 Moved Swagger to commons

--- src/main/java/com/odysseusinc/arachne/portal/config/SwaggerConfig.java
+++ src/main/java/com/odysseusinc/arachne/portal/config/SwaggerConfig.java
@@ -22,24 +22,13 @@
 
 package com.odysseusinc.arachne.portal.config;
 
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
+import com.odysseusinc.arachne.commons.config.DocketWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.RequestHandler;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.SecurityScheme;
-import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -63,66 +52,14 @@ public class SwaggerConfig {
     @Bean
     public DocketWrapper docketWrapper() {
 
-        return getDocket("Arachne Central",
+        return new DocketWrapper("Arachne Central",
                 "Arachne Central API",
                 "1.0.0",
                 "",
                 arachneTokenHeader,
+                RestController.class,
                 "com.odysseusinc.arachne.portal.api.v1.controller"
         );
-    }
-
-    public static DocketWrapper getDocket(
-            String title,
-            String description,
-            String version,
-            String license,
-            String tokenHeader,
-            String... packages) {
-
-        List<SecurityScheme> securitySchemes = Arrays.asList(new ApiKey(tokenHeader, "api_key", "header"));
-
-        final Predicate<RequestHandler> packagePredicate = Stream.of(packages)
-                .map(RequestHandlerSelectors::basePackage)
-                .reduce(Predicates::or)
-                .orElse(x -> false);
-
-        Predicate<RequestHandler> annotationPredicate = RequestHandlerSelectors.withClassAnnotation(RestController.class);
-
-        Predicate<RequestHandler> selectorPredicate = Stream.of(packagePredicate, annotationPredicate)
-                .reduce(Predicates::and)
-                .orElse(x -> true);
-
-        return new DocketWrapper(new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(selectorPredicate)
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(new ApiInfoBuilder()
-                        .title(title)
-                        .license(license)
-                        .description(description)
-                        .version(version)
-                        .build()
-                )
-                .securitySchemes(securitySchemes)
-                .pathMapping("/")
-                .useDefaultResponseMessages(false));
-    }
-
-    public static class DocketWrapper {
-
-        private Docket docket;
-
-        public DocketWrapper(Docket docket) {
-
-            this.docket = docket;
-        }
-
-        public Docket getDocket() {
-
-            return docket;
-        }
     }
 
 }
