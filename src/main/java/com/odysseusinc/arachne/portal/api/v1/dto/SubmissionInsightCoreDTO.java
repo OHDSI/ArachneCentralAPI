7f48555fe453ba81d736cb1658d9e8d15bba44a2
Pavel Grafkin <pavgra@gmail.com>
2017-11-13 13:53:09 +0300

    ARACHNE-1570. Fixed update of Insight description

---
+++ src/main/java/com/odysseusinc/arachne/portal/api/v1/dto/SubmissionInsightCoreDTO.java
@@ -0,0 +1,10 @@
+package com.odysseusinc.arachne.portal.api.v1.dto;
+
+public abstract class SubmissionInsightCoreDTO {
+
+    public abstract String getName();
+    public abstract void setName(String name);
+
+    public abstract String getDescription();
+    public abstract void setDescription(String description);
+}
