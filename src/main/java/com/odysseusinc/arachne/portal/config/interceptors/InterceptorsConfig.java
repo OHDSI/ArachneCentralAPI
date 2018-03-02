e71745c24d9e36c5bce278550de0901b0c3d4142
Anton Gackovka <anton.gackovka@odysseusinc.com>
2018-03-02 16:04:48 +0300

    ARACHNE-1994 reverted config

--- src/main/java/com/odysseusinc/arachne/portal/config/interceptors/InterceptorsConfig.java
+++ src/main/java/com/odysseusinc/arachne/portal/config/interceptors/InterceptorsConfig.java
@@ -20,25 +20,13 @@
 
 package com.odysseusinc.arachne.portal.config.interceptors;
 
-import javax.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 @Configuration
 @ConditionalOnProperty("portal.useQueryInterceptor")
 public class InterceptorsConfig {
-
-    @Autowired
-    private LocalContainerEntityManagerFactoryBean entityManagerFactory;
-
-    @PostConstruct
-    public void init() {
-
-        entityManagerFactory.getJpaPropertyMap().put("hibernate.ejb.interceptor", queryInterceptor());
-    }
 
     @Bean
     public QueryInterceptor queryInterceptor() {
