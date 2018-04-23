46cd2f25a75522af606e41f25f90312991a571d6
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2018-04-23 16:54:46 +0300

    review fix

--- src/main/java/com/odysseusinc/arachne/portal/service/impl/BaseAtlasServiceImpl.java
+++ src/main/java/com/odysseusinc/arachne/portal/service/impl/BaseAtlasServiceImpl.java
@@ -50,7 +50,7 @@ public class BaseAtlasServiceImpl<T extends IAtlas> implements BaseAtlasService<
     }
 
     @Override
-    @PostAuthorize("@dataNodeRepository.findOne(#dataNodeId) == authentication.principal || hasRole('ROLE_ADMIN')")
+    @PreAuthorize("@dataNodeRepository.findOne(#dataNodeId) == authentication.principal || hasRole('ROLE_ADMIN')")
     public T findByNameAndNodeInAnyTenant(String name, Long dataNodeId) {
 
         return baseRawAtlasRepository.findByNameAndDataNodeId(name, dataNodeId);
