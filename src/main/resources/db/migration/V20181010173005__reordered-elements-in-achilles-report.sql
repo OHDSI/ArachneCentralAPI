20fcc131b9348118e77bd1b352f513d987e8f128
Anastasiia Klochkova <anastasiia.klochkova@odysseusinc.com>
2018-11-30 17:42:02 +0300

    ARACHNE-2563. Added 'sort order' to AchillesReportDTO; changed order of elements in report

---
+++ src/main/resources/db/migration/V20181010173005__reordered-elements-in-achilles-report.sql
@@ -0,0 +1,55 @@
+UPDATE achilles_reports
+SET sort_order = 10
+WHERE label = 'dashboard';
+
+UPDATE achilles_reports
+SET sort_order = 20
+WHERE label = 'datadensity';
+
+UPDATE achilles_reports
+SET sort_order = 30
+WHERE label = 'person';
+
+UPDATE achilles_reports
+SET sort_order = 40
+WHERE label = 'visits';
+
+UPDATE achilles_reports
+SET sort_order = 50
+WHERE label = 'conditions';
+
+UPDATE achilles_reports
+SET sort_order = 60
+WHERE label = 'conditionera';
+
+UPDATE achilles_reports
+SET sort_order = 70
+WHERE label = 'procedures';
+
+UPDATE achilles_reports
+SET sort_order = 80
+WHERE label = 'drugexposures';
+
+UPDATE achilles_reports
+SET sort_order = 90
+WHERE label = 'drugeras';
+
+UPDATE achilles_reports
+SET sort_order = 100
+WHERE label = 'measurement';
+
+UPDATE achilles_reports
+SET sort_order = 110
+WHERE label = 'observations';
+
+UPDATE achilles_reports
+SET sort_order = 120
+WHERE label = 'observationperiods';
+
+UPDATE achilles_reports
+SET sort_order = 130
+WHERE label = 'death';
+
+UPDATE achilles_reports
+SET sort_order = 140
+WHERE label = 'achillesheel';
