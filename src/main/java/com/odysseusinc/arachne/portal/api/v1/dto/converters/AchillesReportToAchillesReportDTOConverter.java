0a834f563bf1d6045f799245710e81a26e7c676b
anastasiia klochkova <38942654+aklochkova@users.noreply.github.com>
2018-11-30 17:43:00 +0300

    ARACHNE-2563. Changed order of elements in achilles report (#556)

--- src/main/java/com/odysseusinc/arachne/portal/api/v1/dto/converters/AchillesReportToAchillesReportDTOConverter.java
+++ src/main/java/com/odysseusinc/arachne/portal/api/v1/dto/converters/AchillesReportToAchillesReportDTOConverter.java
@@ -24,8 +24,6 @@ package com.odysseusinc.arachne.portal.api.v1.dto.converters;
 
 import com.odysseusinc.arachne.portal.api.v1.dto.AchillesReportDTO;
 import com.odysseusinc.arachne.portal.model.achilles.AchillesReport;
-import com.odysseusinc.arachne.portal.api.v1.dto.converters.BaseConversionServiceAwareConverter;
-import org.springframework.core.convert.support.GenericConversionService;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -37,6 +35,7 @@ public class AchillesReportToAchillesReportDTOConverter extends BaseConversionSe
         AchillesReportDTO dto = new AchillesReportDTO();
         dto.setLabel(report.getLabel());
         dto.setName(report.getName());
+        dto.setSortOrder(report.getSortOrder());
         return dto;
     }
 }
