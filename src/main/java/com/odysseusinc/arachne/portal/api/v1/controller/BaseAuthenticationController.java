336b608f25f1ec5ca9a5ec59a33a37cae441e8f5
anastasiia klochkova <38942654+aklochkova@users.noreply.github.com>
2018-12-24 13:05:11 +0300

    ARACHNE-2610. Replaced string message with constant (#577)

--- src/main/java/com/odysseusinc/arachne/portal/api/v1/controller/BaseAuthenticationController.java
+++ src/main/java/com/odysseusinc/arachne/portal/api/v1/controller/BaseAuthenticationController.java
@@ -28,6 +28,7 @@ import com.odysseusinc.arachne.commons.api.v1.dto.CommonAuthMethodDTO;
 import com.odysseusinc.arachne.commons.api.v1.dto.CommonAuthenticationRequest;
 import com.odysseusinc.arachne.commons.api.v1.dto.CommonAuthenticationResponse;
 import com.odysseusinc.arachne.commons.api.v1.dto.util.JsonResult;
+import com.odysseusinc.arachne.commons.utils.ErrorMessages;
 import com.odysseusinc.arachne.portal.api.v1.dto.RemindPasswordDTO;
 import com.odysseusinc.arachne.portal.api.v1.dto.ResetPasswordDTO;
 import com.odysseusinc.arachne.portal.api.v1.dto.UserInfoDTO;
@@ -168,7 +169,7 @@ public abstract class BaseAuthenticationController extends BaseController<DataNo
 
         IUser user = userService.getByEmailInAnyTenant(email);
         if (user == null) {
-            throw new BadCredentialsException("Bad credentials");
+            throw new BadCredentialsException(ErrorMessages.BAD_CREDENTIALS.getMessage());
         }
         if (user.getTenants() == null || user.getTenants().isEmpty()) {
             throw new NoDefaultTenantException("Request admin to add you into a tenant.");
