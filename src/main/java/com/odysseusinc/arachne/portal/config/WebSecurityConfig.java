e826a99fa7eb210e4913c266ced75df881ae9825
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-08-16 19:26:14 +0300

    optimize imports

--- src/main/java/com/odysseusinc/arachne/portal/config/WebSecurityConfig.java
+++ src/main/java/com/odysseusinc/arachne/portal/config/WebSecurityConfig.java
@@ -59,7 +59,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
