77dfe70a0f13935281007d52cd3d324bd61303bc
anton-abushkevich <anton.abushkevich@gmail.com>
2020-10-23 17:42:51 +0300

    Study title in the name of downloaded code archive (#702)
    * Study title in the name of downloaded code archive
    
    * ARACHNE-2830 Add possibility to add comment while inviting user in Study
    
    * ARACHNE-2830 Add possibility to add comment while inviting user in Study
    
    * ARACHNE-2830 Add possibility to add comment while inviting user in Study
    
    * Study title in the name of downloaded code archive
    
    Co-authored-by: anton.abushkevich <anton.abushkevich>

--- src/main/java/com/odysseusinc/arachne/portal/api/v1/controller/BaseStudyController.java
+++ src/main/java/com/odysseusinc/arachne/portal/api/v1/controller/BaseStudyController.java
@@ -33,6 +33,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 import com.odysseusinc.arachne.commons.api.v1.dto.util.JsonResult;
+import com.odysseusinc.arachne.commons.utils.CommonFilenameUtils;
 import com.odysseusinc.arachne.commons.utils.UserIdUtils;
 import com.odysseusinc.arachne.portal.api.v1.dto.AddStudyParticipantDTO;
 import com.odysseusinc.arachne.portal.api.v1.dto.BooleanDTO;
@@ -481,9 +482,10 @@ public abstract class BaseStudyController<
             @PathVariable("studyId") Long studyId,
             HttpServletResponse response) throws PermissionDeniedException, NotExistException, IOException {
 
-        String archiveName = "study_" + studyId + "_"
-                + Long.toString(System.currentTimeMillis())
-                + ".zip";
+        final Study study = studyService.getById(studyId);
+
+        String archiveName = "documents_" + CommonFilenameUtils.sanitizeFilename(study.getTitle()) + ".zip";
+
         String contentType = "application/zip, application/octet-stream";
         response.setContentType(contentType);
         response.setHeader("Content-type", contentType);
