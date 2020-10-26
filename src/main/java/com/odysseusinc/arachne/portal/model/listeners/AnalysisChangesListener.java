2c3eef481930e90eb3920f3cc2dfc1e882fd4568
Alex Cumarav <7705583+acumarav@users.noreply.github.com>
2020-10-26 10:08:45 +0100

    refresh study Update date on change in analysis and analysis code (#699)
    * Update study modification date on analysis or analysisFile changes
    
    * Update study modification date on analysis or analysisFile changes
    
    * minor cleanup fixes

---
+++ src/main/java/com/odysseusinc/arachne/portal/model/listeners/AnalysisChangesListener.java
@@ -0,0 +1,24 @@
+package com.odysseusinc.arachne.portal.model.listeners;
+
+import com.odysseusinc.arachne.portal.model.Analysis;
+import com.odysseusinc.arachne.portal.model.AnalysisFile;
+
+import javax.persistence.PrePersist;
+import javax.persistence.PreRemove;
+import javax.persistence.PreUpdate;
+import java.util.Date;
+
+public class AnalysisChangesListener {
+
+    @PreUpdate
+    @PrePersist
+    @PreRemove
+    public void update(Object entity) {
+
+        if (entity instanceof Analysis) {
+            ((Analysis) entity).getStudy().setUpdated(new Date());
+        } else if (entity instanceof AnalysisFile) {
+            ((AnalysisFile) entity).getAnalysis().getStudy().setUpdated(new Date());
+        }
+    }
+}
