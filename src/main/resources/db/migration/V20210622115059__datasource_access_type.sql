64f8931763dc3c6c49f81affa90db9fad5e4ec1e
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2021-07-21 14:39:55 +0300

    Data source view is dropped before creation (https://github.com/OHDSI/ArachneUI/issues/825)

--- src/main/resources/db/migration/V20210622115059__datasource_access_type.sql
+++ src/main/resources/db/migration/V20210622115059__datasource_access_type.sql
@@ -6,6 +6,8 @@ ALTER TABLE data_sources_data DROP CONSTRAINT data_sources_data_not_blank_fields
 ALTER TABLE data_sources_data ADD CONSTRAINT data_sources_data_not_blank_fields_if_published
     CHECK (published = FALSE OR (organization <> '' AND model_type <> '' AND access_type <> ''));
 
+DROP VIEW IF EXISTS data_sources;
+
 CREATE OR REPLACE VIEW data_sources
 AS
 SELECT ds.id,
