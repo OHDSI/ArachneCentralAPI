1a8b67f4f40fc907310e7647775c2726b96293a7
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2021-10-05 13:34:14 +0300

    Fixed after review(https://github.com/OHDSI/ArachneCentralAPI/issues/745)

--- src/main/java/com/odysseusinc/arachne/portal/util/SubmissionHelper.java
+++ src/main/java/com/odysseusinc/arachne/portal/util/SubmissionHelper.java
@@ -40,8 +40,6 @@ import com.odysseusinc.arachne.storage.service.JcrContentStorageServiceImpl;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
@@ -50,31 +48,23 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.Reader;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 import static com.odysseusinc.arachne.storage.service.ContentStorageService.PATH_SEPARATOR;
 
@@ -329,18 +319,21 @@ public class SubmissionHelper {
             try {
                 final String resultsDir = contentStorageHelper.getResultFilesDir(submission);
                 final Map<String, String> cohortNames = new HashMap<>();
-                ArachneFileMeta studySpec = findAnalysisSpec(submission);
-                if (Objects.nonNull(studySpec)) {
-                    JsonParser parser = new JsonParser();
-                    try (InputStream fileIn = contentStorageService.getContentByFilepath(studySpec.getPath());
-                         Reader json = new InputStreamReader(fileIn)) {
-                        JsonObject spec = parser.parse(json).getAsJsonObject();
-                        JsonArray targets = spec.get("targetCohorts").getAsJsonArray();
-                        cohortNames.putAll(getCohortNames(targets));
-                        JsonArray outcomes = spec.get("outcomeCohorts").getAsJsonArray();
-                        cohortNames.putAll(getCohortNames(outcomes));
-                    }
-                }
+                Optional<ArachneFileMeta> studySpec = findAnalysisSpec(submission);
+                studySpec
+                        .ifPresent(study -> {
+                            JsonParser parser = new JsonParser();
+                            try (InputStream fileIn = contentStorageService.getContentByFilepath(study.getPath());
+                                 Reader json = new InputStreamReader(fileIn)) {
+                                JsonObject spec = parser.parse(json).getAsJsonObject();
+                                JsonArray targets = spec.get("targetCohorts").getAsJsonArray();
+                                cohortNames.putAll(getCohortNames(targets));
+                                JsonArray outcomes = spec.get("outcomeCohorts").getAsJsonArray();
+                                cohortNames.putAll(getCohortNames(outcomes));
+                            } catch (IOException e) {
+                                LOGGER.debug(CAN_NOT_PARSE_LOG, INCIDENCE_SUMMARY_FILENAME);
+                            }
+                        });
 
                 ArachneFileMeta arachneFile = contentStorageService.getFileByPath(resultsDir
                         + PATH_SEPARATOR
@@ -412,9 +405,9 @@ public class SubmissionHelper {
             submission.setResultInfo(result);
         }
 
-        private ArachneFileMeta findAnalysisSpec(Submission submission) {
+        private Optional<ArachneFileMeta> findAnalysisSpec(Submission submission) {
             List<ArachneFileMeta> files = searchFiles(submission, STUDY_SPECIFICATION_JSON);
-            return files.isEmpty() ? null : files.get(0);
+            return files.stream().findFirst();
         }
 
         private Map<String, String> getCohortNames(JsonArray cohorts) {
